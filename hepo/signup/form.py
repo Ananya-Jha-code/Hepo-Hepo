4663625318f15ce1797491626c801542081c9d0e
Prakhar <prakharsing7@gmail.com>
2021-05-29 17:53:23 +0530

    yaas

--- hepo/signup/form.py
+++ hepo/signup/form.py
@@ -6,8 +6,18 @@ from django import forms
 
 
 class CreateUserForm(UserCreationForm):
+    username = forms.CharField(max_length=30, required=True, widget=forms.TextInput
+    (attrs={'placeholder': 'Enter A Username...'}))
+
+    email = forms.CharField(max_length=30, required=True, widget=forms.EmailInput
+    (attrs={'placeholder':'Enter Your Email'}))
+
+    password1 = forms.CharField(max_length=30, required=True, widget=forms.PasswordInput
+    (attrs={'placeholder': 'Enter Your Password'}))
+
+    password2 = forms.CharField(max_length=30, required=True, widget=forms.PasswordInput
+    (attrs={'placeholder': 'Enter Password Again'}))
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-
-
